7b880d7df5c88d7210c0a1ef686ab8db910716e6
David E Lares S <david.e.lares@gmail.com>
2019-01-02 23:59:45 -0600

    Nuxt Hello, World example

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template lang="html">
+  <h1>Hello, World</h1>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage' // component name
+}
+</script>
+
+<style lang="css" scoped>
+</style>
