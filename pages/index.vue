6c06d489482af628dc7df9274abfd165686be081
David E Lares S <david.e.lares@gmail.com>
2019-01-03 00:10:48 -0600

    Axios Endpoint config, axios installation, and component hook

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,23 @@
 </template>
 
 <script>
+import axios from 'axios'
+import env from '../config/env'
 export default {
-  name: 'IndexPage' // component name
+  name: 'IndexPage', // component name
+  data(){
+    return {
+      albums: {}
+    }
+  },
+  created(){
+    // VueJS Hook
+    axios.get(`${env.endpoint}/albums`)
+    .then(res => {
+      // client and also displayed on server side
+      console.log(res)
+    })
+  }
 }
 </script>
 
